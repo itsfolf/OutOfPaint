841285f55fd3bbe815e0f42306ad4ac1dd662751
Checkium <checkium@gmail.com>
2019-01-26 17:29:00 +0000

    Fixed string change checker and added a thing to revert patched methods so that patched strings dont go bork

--- src/main/java/me/checkium/outofpaint/processors/DeobfuscateStringsProcessor.java
+++ src/main/java/me/checkium/outofpaint/processors/DeobfuscateStringsProcessor.java
@@ -1,6 +1,5 @@
 package me.checkium.outofpaint.processors;
 
-import me.checkium.outofpaint.OutOfPaint;
 import me.checkium.outofpaint.utils.methodexecutor.executor.Context;
 import me.checkium.outofpaint.utils.methodexecutor.executor.MethodExecutor;
 import me.checkium.outofpaint.utils.methodexecutor.executor.defined.JVMComparisonProvider;
@@ -27,11 +26,14 @@ public class DeobfuscateStringsProcessor {
         }
         int amount = 0;
         Map<String, MethodNode> decryptMethodCache = new HashMap<>();
+        Map<String, InsnList> originalDecryptMethodCache = new HashMap<>();
+
         for (ClassNode classNode : classNames.values()) {
             for (MethodNode method : classNode.methods) {
                 for (AbstractInsnNode abstractInsnNode : method.instructions.toArray()) {
                     if (abstractInsnNode instanceof LdcInsnNode && ((LdcInsnNode) abstractInsnNode).cst instanceof String) {
                         String encryptedString = (String) ((LdcInsnNode) abstractInsnNode).cst;
+                        String decryptedString = null;
                         String decryptKey = classNode.name.replaceAll("/", ".") + method.name.replaceAll("/", ".");
                         MethodInsnNode decryptCall;
                         if (abstractInsnNode.getNext() instanceof MethodInsnNode) {
@@ -44,11 +46,21 @@ public class DeobfuscateStringsProcessor {
                                     for (MethodNode decryptClassMethod : decryptClass.methods) {
                                         if (decryptClassMethod.name.equals(decryptMethodName) && decryptClassMethod.desc.equals(decryptCall.desc)) {
                                             String methodLoc = decryptMethodOwner + " " + decryptMethodName;
-                                            if (decryptMethodCache.containsKey(methodLoc)) {
-                                                ((LdcInsnNode) abstractInsnNode).cst = getString(encryptedString, decryptClass, decryptMethodCache.get(methodLoc), decryptKey);
-                                                method.instructions.remove(decryptCall);
-                                                amount++;
-                                            } else {
+                                            if (!decryptMethodCache.containsKey(methodLoc)) {
+                                                InsnList copy = new InsnList();
+                                                InsnList original = decryptClassMethod.instructions;
+
+                                                HashMap<LabelNode, LabelNode> labelMap = new HashMap<>();
+                                                for (AbstractInsnNode insn = original.getFirst(); insn != null; insn = insn.getNext()) {
+                                                    if (insn.getType() == 8) {
+                                                        labelMap.put((LabelNode) insn, new LabelNode());
+                                                    }
+                                                }
+                                                for (AbstractInsnNode insnNode : original.toArray()) {
+                                                    copy.add(insnNode.clone(labelMap));
+                                                }
+                                                originalDecryptMethodCache.put(methodLoc, copy);
+
                                                 int current = 0;
                                                 for (AbstractInsnNode insnNode : decryptClassMethod.instructions.toArray()) {
                                                     if (current > 0) decryptClassMethod.instructions.remove(insnNode);
@@ -61,12 +73,14 @@ public class DeobfuscateStringsProcessor {
                                                 toAdd.add(new LdcInsnNode("toChange"));
                                                 decryptClassMethod.instructions.insert(decryptClassMethod.instructions.getFirst(), toAdd);
                                                 decryptMethodCache.put(methodLoc, decryptClassMethod);
-                                                ((LdcInsnNode) abstractInsnNode).cst = getString(encryptedString, decryptClass, decryptMethodCache.get(methodLoc), decryptKey);
-                                                method.instructions.remove(decryptCall);
-                                                amount++;
                                             }
+                                            decryptedString = getString(encryptedString, decryptClass, decryptMethodCache.get(methodLoc), decryptKey);
+                                            ((LdcInsnNode) abstractInsnNode).cst = decryptedString;
+                                            method.instructions.remove(decryptCall);
+                                            amount++;
                                         }
-                                        if (encryptedString.equals(((LdcInsnNode) abstractInsnNode).cst)) {
+
+                                        if (encryptedString == null || encryptedString.equals(decryptedString)) {
                                             System.out.println("Warning: String " + encryptedString + " at " + decryptKey + " didn't change during decryption.");
                                         }
                                     }
@@ -82,6 +96,12 @@ public class DeobfuscateStringsProcessor {
             }
         }
         log("   Deobfuscated " + amount + " strings.");
+        log("   Reverting patched methods...");
+        decryptMethodCache.forEach((s, methodNode) -> {
+            InsnList original = originalDecryptMethodCache.get(s);
+            methodNode.instructions.clear();
+            methodNode.instructions.add(original);
+        });
     }
 
     private String getString(String encryptedString, ClassNode decrypterClass, MethodNode decrypterMethod, String decryptKey) {
@@ -91,7 +111,6 @@ public class DeobfuscateStringsProcessor {
         DelegatingProvider provider = new DelegatingProvider();
         provider.register(new JVMMethodProvider());
         provider.register(new JVMComparisonProvider());
-        //provider.register(new MappedMethodProvider(classes));
         provider.register(new MappedFieldProvider());
         Context context = new Context(provider);
 
